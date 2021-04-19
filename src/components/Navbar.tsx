4833568ef5ce63918dded8819f79697aad7d4d0e
Greg <gregory.rolwes@slu.edu>
2021-04-19 14:56:19 -0500

    Configured tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export interface NavbarProps {
  
 const Navbar: React.SFC<NavbarProps> = () => {
     return ( 
-        <div className="navbar gradient"></div>
+        <div className="blue-gradient fixed items-center top-0 left-0 h-screen w-28 p-2"></div>
     );
 }
  
