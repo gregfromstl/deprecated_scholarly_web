dd53163f9afa07ad6289279964152e8ba2f44472
Greg <gregory.rolwes@slu.edu>
2021-04-20 17:26:15 -0500

    Arxiv API Queries failing when too long Fixes #1

--- src/services/ArxivService.tsx
+++ src/services/ArxivService.tsx
@@ -2,8 +2,22 @@ import Paper from '../types/Paper';
 
 class ArxivService {
     public async queryArxiv(terms: string[]): Promise<Paper[]> {
-        var query = terms.join("+OR+").replaceAll(" ", "\\ ");
-        console.log(`http://export.arxiv.org/api/query?search_query=all:${query}`);
+        var all_results: Paper[] = [];
+        for (var i = 0; i < terms.length; i++) {
+            var result = await this.querySingleTerm(terms[i]);
+            all_results = [
+                ...all_results,
+                ...result
+            ]
+        }
+        console.log(all_results);
+        // shuffle results
+        return all_results.sort(() => Math.random() - 0.5);
+    }
+
+    private async querySingleTerm(term: string): Promise<Paper[]> {
+        var query = term.replaceAll(" ", "\\ ");
+        console.log(query);
         var results = await fetch(`http://export.arxiv.org/api/query?search_query=all:${query}`)
             .then(res => res.text())
             .then(
@@ -13,7 +27,7 @@ class ArxivService {
                         return (
                             {
                                 title: entry.filter((val: any) => val.name === "title")[0].value,
-                                abstract: entry.filter((val: any) => val.name === "summary")[0].value
+                                summary: entry.filter((val: any) => val.name === "summary")[0].value
                             }
                         )
                     })
