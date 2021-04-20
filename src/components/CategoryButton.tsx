b7faf4f4310e74be08894007563857c1da5a229c
Greg <gregory.rolwes@slu.edu>
2021-04-19 22:44:17 -0500

    Added 3 more categories

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -8,7 +8,7 @@ export interface CategoryButtonProps {
  
 const CategoryButton: React.SFC<CategoryButtonProps> = (props) => {
     return ( 
-        <button onClick={() => props.onClick(props.category)} className={`text-base items-center flex flex-col ${ props.active ? "" : "opacity-50" }`}>
+        <button onClick={() => props.onClick(props.category)} className={`text-base items-center flex flex-col mt-12 ${ props.active ? "" : "opacity-50" }`}>
             {props.children}
         </button>
     );
--- src/components/CategoryPanel.tsx
+++ src/components/CategoryPanel.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import '../assets/camera.svg';
 import { ReactComponent as Camera } from '../assets/camera.svg';
+import { ReactComponent as Finance } from '../assets/finance.svg';
+import { ReactComponent as Robot } from '../assets/robot.svg';
+import { ReactComponent as Brain } from '../assets/brain.svg';
 import CategoryButton from './CategoryButton';
 
 export interface CategoryPanelProps {
     selectCategory: Function,
-    terms: Array<string>
+    terms: string[]
 }
  
 const CategoryPanel: React.SFC<CategoryPanelProps> = (props) => {
     return ( 
         <div className="shadow-right bg-white fixed w-72 h-screen text-2xl font-light top-0 left-28 p-8">
             Categories
-            <div className="categories text-sm items-center flex flex-col mt-12">
+            <div className="categories text-sm items-center flex flex-col">
                 <CategoryButton onClick={ props.selectCategory } category="computer vision" active={props.terms.includes("computer vision")}>
                     <Camera className="w-12" />
                     Computer Vision
+                </CategoryButton>
+                <CategoryButton onClick={ props.selectCategory } category="quantitative finance" active={props.terms.includes("quantitative finance")}>
+                    <Finance className="w-12" />
+                    Finance
+                </CategoryButton>
+                <CategoryButton onClick={ props.selectCategory } category="robotics" active={props.terms.includes("robotics")}>
+                    <Robot className="w-12" />
+                    Robotics
+                </CategoryButton>
+                <CategoryButton onClick={ props.selectCategory } category="quantitative biology neuroscience" active={props.terms.includes("quantitative biology neuroscience")}>
+                    <Brain className="w-12" />
+                    Neuroscience
                 </CategoryButton>
             </div>
         </div>
