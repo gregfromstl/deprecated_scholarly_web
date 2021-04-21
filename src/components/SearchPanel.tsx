e1cd4d2c1e40a76b986b4f5c32010039f4aa3542
Greg <gregory.rolwes@slu.edu>
2021-04-21 11:46:04 -0500

    Added Open Sans font

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -26,20 +26,19 @@ class SearchPanel extends React.Component<SearchPanelProps, SearchPanelState> {
     }
 
     render() { 
-        //w-3/4 sm:w-7/12 md:w-1/3 lg:w-1/4 xl:w-2/12
         return (
             <div className="shadow-bottom w-screen p-4 pr-10 pl-10 flex flex-col md:flex-row justify-between">
                 <div className="flex flex-col m-auto mt-4 mb-4 md:m-0 w-96 md:w-56">
                     <Logo className="m-auto" />
                 </div>
                 <div className="w-11/12 sm:w-10/12 md:w-1/2 lg:w-5/12 xl:w-1/3 m-auto mt-4 mb-4 md:m-0 flex">
-                    <div className="m-auto p-1 w-full flex flex-row border border-r-0 border-gray-light rounded-l-lg">
+                    <div className="font-body text-sm m-auto p-1 w-full flex flex-row border border-r-0 border-gray-light rounded-l-lg">
                         <span className="text-gray-light float-left p-0.5 pt-1 pr-0">
                             <SearchIcon />
                         </span>
                         <input className="pl-2 pr-2 w-full" type="text" placeholder="Find a paper" onChange={(event) => { this.setInput(event.target.value) }} />
                     </div>
-                    <button className="rounded-r-lg p-1 bg-blue text-white w-32 font-light" onClick={ () => this.props.search(this.state.input) }>Search</button>
+                    <button className="text-base rounded-r-lg p-1 bg-blue text-white w-32 font-light" onClick={ () => this.props.search(this.state.input) }>Search</button>
                 </div>
             </div>
         );
