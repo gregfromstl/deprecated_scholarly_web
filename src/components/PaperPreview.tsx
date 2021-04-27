c64c27f72da4fa1bb211933e9ce6e3a3d97c0e27
Greg <gregory.rolwes@slu.edu>
2021-04-27 11:49:17 -0500

    Made summary body test

--- src/components/PaperPreview.tsx
+++ src/components/PaperPreview.tsx
@@ -9,7 +9,7 @@ const PaperPreview: React.SFC<PaperPreviewProps> = (props) => {
     return ( 
         <div className="w-full md:w-10/12 lg:w-7/12 m-auto p-8 md:p-16 border-b border-gray-light overflow-hidden">
             <div className="title text-xl">{ props.title }</div>
-            <div className="summary text-base text-gray">{ props.summary.substring(0,500) + "..." }</div>
+            <div className="summary text-base text-gray font-body">{ props.summary.substring(0,500) + "..." }</div>
         </div>
     );
 }
