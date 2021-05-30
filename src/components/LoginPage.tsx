7483f804b6e31dafdc907ebe62d04e559de2ac24
gregrolwes <gregory.rolwes@slu.edu>
2021-05-29 22:53:40 -0500

    Added placeholder login form

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -3,7 +3,19 @@ const LoginPage = () => {
         <div>
             TEST
             <form>
-                <input />
+                <label htmlFor="fname">email</label>
+                <br />
+                <div className="border-2 border-gray-light w-32">
+                    <input type="text" id="fname" name="fname" />
+                </div>
+                <br />
+                <label htmlFor="lname">password:</label>
+                <br />
+                <div className="border-2 border-gray-light w-32">
+                    <input type="text" id="lname" name="lname" />
+                </div>
+                <br />
+                <button type="submit">Login</button>
             </form>
         </div>
     )
